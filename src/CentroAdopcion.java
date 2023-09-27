43fd9216433b2a38a8f79bb81aaf9007b85bec13
brayamrendon <brayam.rendon10@gmail.com>
2023-09-26 23:06:16 -0500

    Creando las clases donde se va almacenar la informacion ingresada y la clase ProcesoAdopcion

---
+++ src/CentroAdopcion.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CentroAdopcion {
+    private List<Animal> animales = new ArrayList<>();
+    private List<Adoptante> adoptantes = new ArrayList<>();
+    private List<ProcesoAdopcion> adopciones = new ArrayList<>();
+    private List<ProcesoAdopcion> empleados = new ArrayList<>();
+}
--- src/ProcesoAdopcion.java
+++ src/ProcesoAdopcion.java
@@ -1,4 +1,12 @@
 // Clase para registro Proceso de adopcion
 public class ProcesoAdopcion {
+    private Adoptante adoptante;
+    private Animal animal;
+    private String fecha;
 
+    public ProcesoAdopcion(Adoptante adoptante, Animal animal, String fecha) {
+        this.adoptante = adoptante;
+        this.animal = animal;
+        this.fecha = fecha;
+    }
 }
